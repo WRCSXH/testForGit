f5d051183ca3349acd8b0ccd3577813f1cedc09f
zs <zs@bjpowernode.com>
2020-10-16 22:36:36 +0800

    1

--- src/com/bjpowernode/domain/User.java
+++ src/com/bjpowernode/domain/User.java
@@ -5,7 +5,5 @@ package com.bjpowernode.domain;
 public class User {
     private String id;
     private String name;
-    private String phone;
     private String age;
-    private String address;
 }
